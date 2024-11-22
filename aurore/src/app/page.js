03d87acd81dbad0693f1085722bc71a80e9e184c
elpelaoloco <jvergars@uc.cl>
2024-11-22 13:44:09 -0300

    feat: two sections ready and also added a content folder to store the text content used in the page

--- aurore/src/app/page.js
+++ aurore/src/app/page.js
@@ -1,31 +1,71 @@
 import Link from "next/link";
 import styles from "@/styles/Home.module.css";
-import HeroSection from "@/components/HeroSection";
 import ServicesSection from "@/components/ServicesSection";
+import { FirstSection } from "@/content/home";
+import { SecondSection } from "@/content/home";
 
 const Home = () => (
-
-  <div className="container mx-auto" >
-    <HeroSection Title={"Driving Innovation Together"} 
-    Subtitle ={"Transform your business with cutting-edge solutions. At Aurore Labs, we innovate and accelerate technologies aligned with your goals, turning objectives into success."}
-    Paragraph={"Discover how Aurore Labs can unlock new possibilities for your business. Schedule a consultation today and let’s start building your future."}
-    Img = {"Images/Pages/Home/home-Driving_innovation_together_optimized.png"} />
-      <main className="p-8 text-center">
-        <h1 className={styles.title}>
-          Innovative Software Solutions for Your Business
-        </h1>
-        <p className={styles.description}>
-          Welcome to AuRore, where we provide top-notch software services to help your business thrive.
-        </p>
-        <div className={styles.buttonContainer}>
-          <Link href="/services">
-            <button className={`${styles.button} ${styles.buttonPrimary}`}> Services </button>
-          </Link>
-          <Link href="/about">
-            <button className={`${styles.button} ${styles.buttonSecondary}`}> About the company </button>
-          </Link>
-        </div>
-      </main>
+  <div className="container mx-auto">
+    <main className="p-8 text-center">
+    <div className="mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="grid md:grid-cols-2 gap-4 md:gap-8 xl:gap-20 md:items-center">
+          <div>
+              <h1 className={styles.title}>{FirstSection.title}</h1>
+              <h2 className={styles.subtitle}>{FirstSection.subtitle}</h2>
+              <p className={styles.description}>{FirstSection.paragraph}</p>
+              <div className="mt-7  w-full">
+                  <a className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" href={FirstSection.buttonRef}>
+                  Get started
+                  </a>
+              </div>
+          </div>
+          <div className="relative ms-4">
+              <img className="w-full rounded-md" src={FirstSection.img} alt="Hero Image"/>              
+          </div>
+      </div>
+    </div>
+    <div>
+      <h2 className={styles.subtitle}>  Intelligent Automations</h2>
+    </div>
+    <div className="mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="grid md:grid-cols-2 gap-4 md:gap-8 xl:gap-20 md:items-center">
+          <div className="relative ms-4">
+              <img className="w-full rounded-md" src={SecondSection.img} alt="Hero Image"/>              
+          </div>
+          <div>
+              <h1 className={styles.title}>{SecondSection.title}</h1>
+              <h2 className={styles.subtitle}>{SecondSection.subtitle}</h2>
+              <p className={styles.description}>{SecondSection.paragraph}</p>
+              <div className="mt-7  w-full">
+                  <a className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" href={FirstSection.buttonRef}>
+                  Learn More
+                  </a>
+              </div>
+          </div>
+      </div>
+    </div>
+      {/* <h1 className={styles.title}>
+        Innovative Software Solutions for Your Business
+      </h1>
+      <p className={styles.description}>
+        Welcome to AuRore, where we provide top-notch software services to help
+        your business thrive.
+      </p>
+      <div className={styles.buttonContainer}>
+        <Link href="/services">
+          <button className={`${styles.button} ${styles.buttonPrimary}`}>
+            {" "}
+            Services{" "}
+          </button>
+        </Link>
+        <Link href="/about">
+          <button className={`${styles.button} ${styles.buttonSecondary}`}>
+            {" "}
+            About the company{" "}
+          </button>
+        </Link>
+      </div> */}
+    </main>
     <ServicesSection />
   </div>
 );
---
+++ aurore/src/content/home.js
@@ -0,0 +1,16 @@
+export const FirstSection = {
+    title: "Driving Innovation Together",
+    subtitle: "Transform your business with cutting-edge solutions. At Aurore Labs, we innovate and accelerate technologies aligned with your goals, turning objectives into success.",
+    paragraph: "Discover how Aurore Labs can unlock new possibilities for your business. Schedule a consultation today and let’s start building your future.",
+    img: "/Images/Pages/Home/home-Driving_innovation_together_optimized.png",
+    buttonRef:"#",
+  };
+
+export const SecondSection = {
+  title: "",
+  subtitle: "OUR APPROACH",
+  paragraph: "Intelligent Automation is the integration of advanced technologies to optimize existing business operations and create new business opportunities. Our unique approach, team, and solutions open the door to innovation and growth, empowering businesses to seize opportunities in real time, while also streamlining existing operations. In today’s fast-paced landscape, adopting Intelligent Automation is essential to staying ahead of competitors and ensuring long-term success.",
+  img: "/Images/Pages/Home/home-Automationssection.jpeg",
+  buttonRef:"",
+}
+  
