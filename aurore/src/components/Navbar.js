6fcdfd8259b3e921f9eef99dfc956ea1ccc4f799
elpelaoloco <jvergars@uc.cl>
2024-11-22 14:02:05 -0300

    feat fix rows behavior and added stles to  styles/global

--- aurore/src/components/Navbar.js
+++ aurore/src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     <header className="sticky top-4 inset-x-0  flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full">
         <nav className="relative max-w-[66rem] w-full bg-transparent stroke-white border border-white rounded-[28px] py-3 ps-5 pe-2 md:flex md:items-center md:justify-between md:py-0 mx-2 lg:mx-auto" aria-label="Global">
           <div className="flex items-center justify-between">
-            <a className="flex-none rounded-md text-xl inline-block font-semibold focus:outline-none focus:opacity-80" href="../../templates/agency/index.html" aria-label="Preline">
+            <a className="flex-none rounded-md text-xl inline-block font-semibold focus:outline-none focus:opacity-80" href="/" aria-label="Preline">
               <img className="w-28 h-auto" src="/Images/Branding/Logo.png" alt="Preline"/>
             </a> 
   
