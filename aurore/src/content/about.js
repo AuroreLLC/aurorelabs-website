b297ace62de735f1416f95ec7efd27461199ecf9
elpelaoloco <jvergars@uc.cl>
2024-11-25 16:45:45 -0300

    feat/ about page ready, needs linked in icon and animations, but is no priority

---
+++ aurore/src/content/about.js
@@ -0,0 +1,56 @@
+export const Main = {
+  title: "Transforming Ideas into Reality",
+  description:
+    "Empowering businesses with innovative solutions that drive growth and inspire change.",
+  button: "Know More",
+};
+export const FirstSection = {
+  subtitle: "Mission and Vision",
+  description:
+    "Leaders in AI-driven innovation, providing automation, software, and digitalization solutions, helping our clients accelerate their growth, reduce costs, and thrive in a competitive environment, while fostering a high-performance culture within our team.",
+  img: "/Images/Pages/AboutUs/0836e69e-89ae-43a1-9dc0-7a282d9fd9a9_optimized.png",
+};
+export const SecondSection = {
+  subtitle: "Core Values",
+  img: "/Images/Pages/AboutUs/Core.png",
+};
+export const ThirdSection = {
+  subtitle: "About Us",
+  description:
+    "At Aurore, we are passionate about driving innovation and delivering cutting-edge solutions that empower businesses to thrive in an ever-evolving technological landscape. Our mission is to transform challenges into opportunities, enabling our clients to stay ahead of the curve with customized, high-impact digital solutions.We believe in the power of collaboration, combining our expertise with the latest technologies to create meaningful and sustainable growth for our clients. Our core values—excellence, adaptability, and integrity—guide every project we undertake, ensuring that we deliver results that exceed expectations.Our diverse team of experts is dedicated to providing exceptional service and innovative strategies that address the unique needs of each market we serve. Whether it's digital transformation, automation, or start-up acceleration, we are committed to helping our clients succeed and achieve their goals.",
+};
+export const FourthSection = {
+  subtitle: "Meet the brilliant Minds",
+  description: "Turning Posibilities Into Progress.",
+  secondSubtitle: "Leadership team",
+};
+export const Team = [
+  {
+    key: 1,
+    name: "Nathaniel Fry",
+    role: "Founder,Ceo",
+    img: "/Images/Pages/AboutUs/Team/Nat.jpg",
+    LinkedIn: "https://www.linkedin.com/in/nattfry/",
+  },
+  {
+    key: 2,
+    name: "Pamela Fry",
+    role: "Founder, CCO",
+    img: "/Images/Pages/AboutUs/Team/pam.jpg",
+    LinkedIn: "https://www.linkedin.com/in/pamela-fry-b02475218/",
+  },
+  {
+    key: 3,
+    name: "Andres Tamm",
+    role: "Founder, CTO",
+    img: "/Images/Pages/AboutUs/Team/Andres.jpg",
+    LinkedIn: "https://www.linkedin.com/in/andrestammhardessen/",
+  },
+  {
+    key: 4,
+    name: "Scott Ahrendt",
+    role: "Founder, C00",
+    img: "/Images/Pages/AboutUs/Team/Scott.jpg",
+    LinkedIn: "https://www.linkedin.com/in/scottahrendt/",
+  },
+];
