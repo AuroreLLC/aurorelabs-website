f73b9b944e75c697491968d3e06f12fac64edde0
amunitac <amunitac@uc.cl>
2024-11-26 09:30:03 -0300

    fix: content exports

--- aurore/src/app/resources/page.js
+++ aurore/src/app/resources/page.js
@@ -1,13 +1,14 @@
 import { cards, subtitles } from "@/content/resources";
 import BlogCards from "@/components/BlogCards";
 import styles from "@/styles/Resources.module.css";
+
 const resources = () => (
   <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
     <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
-      <h2 className={`${Styles.subtitle} md:text-4xl md:leading-tight`}>
+      <h2 className={`${styles.subtitle} md:text-4xl md:leading-tight`}>
         {subtitles.subtitle}
       </h2>
-      <h2 className={`${Styles.subtitle} md:text-4xl md:leading-tight`}>
+      <h2 className={`${styles.subtitle} md:text-4xl md:leading-tight`}>
         {subtitles.secondSubtitle}
       </h2>
     </div>
