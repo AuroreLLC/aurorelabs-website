f9c799661843ee9d84d782a8ae15e491e60751e2
Andrés Tamm <62019639+andres-tamm@users.noreply.github.com>
2024-12-09 06:43:59 -0800

    update(next.config): adding output

--- aurore/next.config.mjs
+++ aurore/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Enables static export
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/aurorelabs-website' : '', // Add your GitHub Pages repo name here
+  trailingSlash: true, // Ensures trailing slashes for compatibility with GitHub Pages
+};
 
 export default nextConfig;
